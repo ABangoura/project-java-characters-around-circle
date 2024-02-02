30a266f12503099afa3ae4222de47c4dae0b7e7f
ABangoura <albangoura@aol.com>
2024-02-01 22:18:56 -0500

    First commit. Setting up project.

---
+++ CharactersAroundCircle/CharactersAroundCircle.java
@@ -0,0 +1,12 @@
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class CharactersAroundCircle extends Application {
+    // Override start method.
+    @Override
+    public void start(Stage primaryStage) {
+        
+    }
+    
+}
